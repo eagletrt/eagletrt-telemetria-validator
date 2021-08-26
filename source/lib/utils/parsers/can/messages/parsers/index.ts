4681ec729a87b6d577d87b8e38984f7d85ba3d8b
mattebit <matteobitussi@gmail.com>
2021-08-26 16:04:23 +0200

    Fixed speed and torque value conversion for inverters

--- source/lib/utils/parsers/can/messages/parsers/index.ts
+++ source/lib/utils/parsers/can/messages/parsers/index.ts
@@ -4,10 +4,12 @@ export type CanMessageParser = (msg: number[]) => any;
 
 export const PARSERS: Record<string, CanMessageParser> = {
     /* INVERTERS */
-    PARSE_INVERTERS_SPEED: msg => zweiComplement((msg[2] << 8) + msg[1]),
+    // (Value / max value) * max rpm
+    PARSE_INVERTERS_SPEED: msg => (zweiComplement((msg[2] << 8) + msg[1]) / 32767) * 7000.0,
     PARSE_INVERTERS_TEMPERATURE_IGBT: msg => ((msg[2] << 8) + msg[1] - 15_797) / 112.1182,
     PARSE_INVERTERS_TEMPERATURE_MOTORS: msg => ((msg[2] << 8) + msg[1] - 9393.9) / 55.1,
-    PARSE_INVERTERS_TORQUE: msg => zweiComplement((msg[2] << 256) + msg[1]) / 55.1,
+    // (Value/max value) = percentage of torque requested
+    PARSE_INVERTERS_TORQUE: msg => zweiComplement((msg[2] << 256) + msg[1]) / 32767.0,
 
     /* BMS_HV */
     PARSE_BMS_HV_VOLTAGE: msg => ({
